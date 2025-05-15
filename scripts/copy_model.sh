3f388110a5e1eab6393a52a27a4204f8c3f8a091
Vladimir Vincan <vladimirvincan@gmail.com>
2025-05-15 11:56:24 +0200

    add download scripts

---
+++ scripts/copy_model.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# how to run: ./copy_model.sh training_id
+
+local="../physionet/"
+echo $1
+ssh vladimir.vincan.ivi@10.68.6.58 "cd ~/physionet/scripts; bash download_model.sh $1"
+scp -r "vladimir.vincan.ivi@10.68.6.58:~/physionet/scripts/$1/best_model.pt" "$local"
