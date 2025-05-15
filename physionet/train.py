4f7a97a92b5e7fc9353724d3fcb5e3b835910757
Vladimir Vincan <vladimirvincan@gmail.com>
2025-05-15 10:13:01 +0200

    fix current_params

--- physionet/train.py
+++ physionet/train.py
@@ -36,15 +36,15 @@ def train_one_epoch(model, dataloader, criterion, scheduler, optimizer,
         batch_end = time.time()
 
         train_loss += loss
-        total_steps += 1
+        current_params['total_steps'] += 1
 
-        writer.add_scalar('train/batch_time', batch_end - batch_start, total_steps)
-        writer.add_scalar('train/lr', scheduler.get_last_lr()[0], total_steps)
+        writer.add_scalar('train/batch_time', batch_end - batch_start, current_params['total_steps'])
+        writer.add_scalar('train/lr', scheduler.get_last_lr()[0], current_params['total_steps'])
 
     epoch_end = time.time()
-    writer.add_scalar('train/epoch_time', epoch_end - epoch_start, epoch)
-    writer.add_scalar('train/loss', train_loss / len(dataloader), epoch)
-    writer.add_scalar('train/total_steps', total_steps, epoch)
+    writer.add_scalar('train/epoch_time', epoch_end - epoch_start, current_params['epoch'])
+    writer.add_scalar('train/loss', train_loss / len(dataloader), current_params['epoch'])
+    writer.add_scalar('train/total_steps', current_params['total_steps'], current_params['epoch'])
 
 
 def validate(model, dataloader, criterion, settings, current_params,
@@ -82,20 +82,20 @@ def validate(model, dataloader, criterion, settings, current_params,
                 score.score_record(label, output, record_name)
             end = time.time()
 
-            total_steps += 1
+            current_params['total_steps'] += 1
 
             writer.add_scalar('validation/batch_time', end - start,
-                              total_steps)
+                              current_params['total_steps'])
 
         auroc_g = score.gross_auroc()
         auprc_g = score.gross_auprc()
         epoch_end = time.time()
 
         print('AUPRC: %f, AUROC: %f' % (auprc_g, auroc_g))
-        writer.add_scalar('validation/AUROC', auroc_g, epoch)
-        writer.add_scalar('validation/AUPRC', auprc_g, epoch)
-        writer.add_scalar('validation/loss', val_loss / len(dataloader), epoch)
-        writer.add_scalar('validation/epoch_time', epoch_end - epoch_start, epoch)
+        writer.add_scalar('validation/AUROC', auroc_g, current_params['epoch'])
+        writer.add_scalar('validation/AUPRC', auprc_g, current_params['epoch'])
+        writer.add_scalar('validation/loss', val_loss / len(dataloader), current_params['epoch'])
+        writer.add_scalar('validation/epoch_time', epoch_end - epoch_start, current_params['epoch'])
 
     return auprc_g
 
@@ -130,15 +130,16 @@ def train_loop(model, train_dataloader, validation_dataloader, settings):
                         optimizer, settings, current_params, writer)
         val_auprc = validate(model, validation_dataloader, criterion, settings,
                              current_params, writer)
+        current_params['epoch'] = epoch
 
-        if best_auprc < val_auprc:
+        if current_params['best_auprc'] < val_auprc:
             print('best auprc: %f, best epoch: %d' % (val_auprc, epoch))
             if os.path.exists(settings['model_name']):
                 os.remove(settings['model_name'])
 
-            best_auprc = val_auprc
+            current_params['best_auprc'] = val_auprc
             # best_model = copy.deepcopy(model)
-            best_epoch = epoch
+            current_params['best_epoch'] = epoch
 
             torch.save(
                 {
