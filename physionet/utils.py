155544c8c40ab78238b0952f8f96252a6560e2c2
Vladimir Vincan <vladimirvincan@gmail.com>
2025-05-19 11:38:59 +0200

    add comparison to create mask, instead of inequality

--- physionet/utils.py
+++ physionet/utils.py
@@ -1,4 +1,25 @@
-def create_mask(labels):
+def create_mask_old(labels):
     mask = labels != -1
     labels = labels[mask]
     return mask
+
+def create_mask(labels):
+    mask = labels > -0.5
+    labels = labels[mask]
+    return mask
+
+
+# TODO: add test to compare create_mask functions
+# tested to work the same
+
+        # print(mask.shape)
+        # print(mask2.shape)
+
+        # num_true = mask.sum()
+        # num_false = (~mask).sum()
+        # num_true05 = mask2.sum()
+        # num_false05 = (~mask2).sum()
+        # print(num_true)
+        # print(num_false)
+        # print(num_true05)
+        # print(num_false05)
